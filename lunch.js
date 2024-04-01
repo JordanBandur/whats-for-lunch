c7c3a14d2ecc689a9cd32dba60301823e24434fd
JordanBandur <jordanbandur@hotmail.ca>
2024-04-01 15:54:08 -0700

    W01D1 What should I do for lunch assignment

--- lunch.js
+++ lunch.js
@@ -14,15 +14,21 @@
  */
 
 const whatToDoForLunch = function(hungry, availableTime) {
-  console.log("I don't know what to do!");
+  if(hungry && availableTime < 20) {
+    console.log("Grab a snack!")
+  } else if (hungry && availableTime >= 20 && availableTime <30) {
+    console.log("Take a break and make some food!")
+  } else if (hungry && availableTime > 30) {
+    console.log("The program is quite intensive. You may want to reconsider...")
+  } else {
+    console.log("you may want to wait till you're hungry.")
+  }
 }
-
 
 /*
  * This is some test runner code that's simply calling our whatToDoForLunch function
  * defined above to verify we're making the right decisions. Do not modify it!
  */
-
 console.log("I'm hungry and I have 20 minutes for lunch.");
 whatToDoForLunch(true, 20);
 console.log("---");
